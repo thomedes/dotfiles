a73f67176794cdbbbbfe5de302b8ac69b836b7f0
root <root@exa-ptg.es>
2013-11-04 10:02:29 +0100

    emacs apache-mode: fix autoload for apache2.conf

--- emacs/apache-mode.el
+++ emacs/apache-mode.el
@@ -606,6 +606,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.htaccess\\'"   . apache-mode))
 (add-to-list 'auto-mode-alist '("httpd\\.conf\\'"  . apache-mode))
+(add-to-list 'auto-mode-alist '("apache2/.*\\.conf\\'"  . apache-mode))
 (add-to-list 'auto-mode-alist '("srm\\.conf\\'"    . apache-mode))
 (add-to-list 'auto-mode-alist '("access\\.conf\\'" . apache-mode))
 (add-to-list 'auto-mode-alist '("sites-\\(available\\|enabled\\)/" . apache-mode))
