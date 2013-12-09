ea14f58df386b72193798192eb96852b72d57049
Toni Homedes <toni@exa-ptg.es>
2013-12-09 12:51:41 +0100

    Fixed makesymlinks

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,26 +8,26 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_logout bash_profile bashrc vimrc"
+files="bash_logout bash_profile bashrc vimrc emacs/emacs"
 
 ##########
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+mkdir -p "$olddir"
 echo "...done"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
-cd $dir
+cd "$dir"
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+echo "Moving any existing dotfiles from ~ to $olddir"
+for file in $files
+do
+    src=~/.${file##*/}
+    [[ -f "$src" ]] && mv "$src" "$olddir/"
+    echo "Creating symlink to '$file' in home directory."
+    ln -s "$dir/$file" "$src"
 done
-
-ln -s ~/dotfiles/ml.xspf ~/.local/share/vlc/ml.xspf # VLC
